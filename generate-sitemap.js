204eeeb62e9bf61bc334ef629fdf01bb7ffe92e1
Elizabeth Walker <hello@hopnestretreats.co.uk>
2026-06-09 17:06:29 +0100

    Refactor to multi-page static routing, add targeted landing pages, and implement advanced SEO/E-E-A-T trust factors

---
+++ generate-sitemap.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const domain = 'https://www.hopnestretreats.co.uk';
+
+// Define priorities for pages
+const priorities = {
+  '/': '1.0',
+  '/tipis': '0.9',
+  '/exclusive-hire-glamping': '0.9',
+  '/faq': '0.8',
+  '/large-group-glamping': '0.7',
+  '/group-glamping-hot-tub': '0.7',
+  '/shepherd-huts': '0.7',
+  '/about': '0.7',
+  '/blog/large-group-glamping-uk-guide': '0.7'
+};
+
+const pages = [
+  '/',
+  '/tipis',
+  '/exclusive-hire-glamping',
+  '/large-group-glamping',
+  '/group-glamping-hot-tub',
+  '/shepherd-huts',
+  '/about',
+  '/faq',
+  '/blog/large-group-glamping-uk-guide'
+];
+
+let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+
+pages.forEach(page => {
+  const priority = priorities[page] || '0.5';
+  xml += '  <url>\n';
+  xml += `    <loc>${domain}${page === '/' ? '' : page}</loc>\n`;
+  xml += `    <priority>${priority}</priority>\n`;
+  xml += '  </url>\n';
+});
+
+xml += '</urlset>\n';
+
+fs.writeFileSync(path.join(__dirname, 'sitemap.xml'), xml);
+console.log('sitemap.xml generated successfully!');
